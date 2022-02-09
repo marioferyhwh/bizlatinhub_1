e947fc5d2cac42742a327612738a5e3617d8671c
mario <marioferyhwh@gmail.com>
2022-02-09 12:26:25 +0000

    renombra archivos de models

--- app/Models/TbAuto.php
+++ app/Models/TbAuto.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tb_user_type extends Model
+class TbAuto extends Model
 {
     use HasFactory;
 }
--- app/Models/TbUser.php
+++ app/Models/TbUser.php
@@ -5,7 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class tb_sell_auto extends Model
+class TbUser extends Model
 {
     use HasFactory;
 }
